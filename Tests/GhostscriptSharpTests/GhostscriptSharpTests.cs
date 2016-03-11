d941760fa378408f94320ec4e0dcc5d304dab657
Mitchell Johnson <orangutandevelopment@gmail.com>
2016-03-11 20:03:42 +1000

    Modified to allow pdf scanning
    Awesomeness

--- Tests/GhostscriptSharpTests/GhostscriptSharpTests.cs
+++ Tests/GhostscriptSharpTests/GhostscriptSharpTests.cs
@@ -1,36 +1,44 @@
 ﻿using System;
-using NUnit.Framework;
 using GhostscriptSharp;
 using System.IO;
+using System.Collections.Generic;
 
 namespace GhostscriptSharpTests
 {
-    [TestFixture]
-    public class GhostscriptSharpTests
+    public static class GhostscriptSharpTests
     {
-        private readonly string TEST_FILE_LOCATION      = "test.pdf";
-        private readonly string SINGLE_FILE_LOCATION    = "output.jpg";
-        private readonly string MULTIPLE_FILE_LOCATION  = "output%d.jpg";
+        private static readonly string TEST_FILE_LOCATION      = "test.pdf";
+        private static readonly string SINGLE_FILE_LOCATION    = "output.jpg";
+        private static readonly string MULTIPLE_FILE_LOCATION  = "output%d.jpg";
 
-        private readonly int MULTIPLE_FILE_PAGE_COUNT = 10;
+        private static readonly int MULTIPLE_FILE_PAGE_COUNT = 10;
 
-        [Test]
-        public void GenerateSinglePageThumbnail()
+        public static void Main(string[] args) {
+            GenerateSinglePageThumbnails();
+        }
+
+        public static void GenerateSinglePageThumbnail()
         {
             GhostscriptWrapper.GeneratePageThumb(TEST_FILE_LOCATION, SINGLE_FILE_LOCATION, 1, 100, 100);
-            Assert.IsTrue(File.Exists(SINGLE_FILE_LOCATION));
         }
 
-        [Test]
-        public void GenerateMultiplePageThumbnails()
+        public static void GenerateSinglePageThumbnails()
         {
-            GhostscriptWrapper.GeneratePageThumbs(TEST_FILE_LOCATION, MULTIPLE_FILE_LOCATION, 1, MULTIPLE_FILE_PAGE_COUNT, 100, 100);
-            for (var i = 1; i <= MULTIPLE_FILE_PAGE_COUNT; i++)
-                Assert.IsTrue(File.Exists(String.Format("output{0}.jpg", i)));
+            DirectoryInfo[] di_subs = (new DirectoryInfo("pdf")).GetDirectories();
+            List<string> pdf_paths = new List<string>();
+            foreach (DirectoryInfo di in di_subs) {
+                FileInfo[] pdfs = di.GetFiles("*.pdf");
+                foreach (FileInfo fi in pdfs)
+                    pdf_paths.Add(fi.FullName);
+            }
+
+            foreach (string s in pdf_paths) {
+                if (!File.Exists(s.Replace(".pdf", ".jpg")))
+                    GhostscriptWrapper.GeneratePageThumb(s, s.Replace(".pdf", ".jpg"), 1, 100, 100);
+            }
         }
 
-        [TearDown]
-        public void Cleanup()
+        public static void Cleanup()
         {
             File.Delete(SINGLE_FILE_LOCATION);
             for (var i = 1; i <= MULTIPLE_FILE_PAGE_COUNT; i++)
